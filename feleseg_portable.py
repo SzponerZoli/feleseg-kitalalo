e11881e18beacfcf2bd5aea24ca624969ade38d6
SzponerZoli <119530750+SzponerZoli@users.noreply.github.com>
2024-07-13 17:46:45 +0200

    Update feleseg_portable.py

--- feleseg_portable.py
+++ feleseg_portable.py
@@ -1,6 +1,17 @@
 from tkinter import *
 from tkinter import ttk
 import tkinter as tk
+win = Tk()
+entry = Entry(win, width= 42)
+def get_data():
+    entered_text = entry.get().strip()
+    if not entered_text or entered_text == placeholder:
+        label.config(text="Először írj be nevet!", fg='red')
+    elif len(entered_text.split()) < 2:
+        label.config(text="A teljes nevedet írd be!", fg='red')
+    else:
+        labeltext = "A feleséged neve: " + entered_text + "né"
+        label.config(text=labeltext, fg='black')
 def on_entry_click(event):
     if entry.get() == placeholder:
         entry.delete(0, "end")
@@ -9,16 +20,9 @@ def on_focusout(event):
     if entry.get() == '':
         entry.insert(0, placeholder)
         entry.config(fg='grey')
-win = Tk()
 win.geometry("700x250")
 win.title("Feleség név kitaláló")
-def get_data():
-    if entry.get() == '' or entry.get() == placeholder:
-        label.config(text="Először írj be nevet!", font= ('Helvetica 11'))
-    else:
-        label.config(text="A feleséged neve: " + entry.get() + "né", font= ('Helvetica 11'))
-placeholder = "Teljes név"
-entry = Entry(win, width= 42)
+placeholder = "Teljes neved"
 entry.place(relx= .5, rely= .5, anchor= CENTER)
 entry.insert(0, placeholder)
 entry.bind('<FocusIn>', on_entry_click)
