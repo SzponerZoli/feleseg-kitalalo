dcf38749779030ef722734adc01b94125bf2c95f
SzponerZoli <119530750+SzponerZoli@users.noreply.github.com>
2024-07-14 15:54:14 +0200

    Update download.bat

--- download.bat
+++ download.bat
@@ -2,7 +2,7 @@
 net session >nul 2>&1
 if %errorLevel% == 0 (
     echo Running with admin privileges.
-    curl -o "C:\Program Files\Szponer\kitalalo\file_neve.exe" https://raw.githubusercontent.com/felhasznalo/repo/master/file_neve.exe
+    curl -o "C:\Program Files\Szponer\kitalalo\feleseg.exe" https://raw.githubusercontent.com/SzponerZoli/feleseg-kitalalo/main/feleseg.exe
 ) else (
     echo Requesting admin privileges...
     powershell start-process elevated_curl_download.bat -verb runas
